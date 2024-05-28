fee4752378e33fd1e4656865f5c961f95abc14e1
denzelbautista <denzel.bautista@utec.edu.pe>
2024-05-27 21:13:00 -0500

    a

--- test_structural.py
+++ test_structural.py
@@ -1,6 +1,17 @@
 import pytest
 import example
 
-def test_can_call_existing_endpoints_of_the_API():
-  ret = example.get_coordinates("Lima,Perú")
-  assert(ret is not None)
+
+def test_cannot_call_existing_endpoints_of_the_API():
+  try:
+    ret = example.get_coordinates("Lima,Perú")
+    assert(ret is not None)
+  except:
+    assert False, "Exception while calling an existing function"
+
+def test_cannot_call_existing_endpoints_of_the_API():
+  try:
+    ret = example.something_not_existent("bla bla")
+    assert False, "Exception not raised"
+  except:
+    pass
