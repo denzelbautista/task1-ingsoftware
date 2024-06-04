cbf8e4b4e937093de5b5b96b041a4c2c58fe3853
denzelbautista <denzel.bautista@utec.edu.pe>
2024-06-03 21:56:23 -0500

    api

--- src/example.py
+++ src/example.py
@@ -4,9 +4,13 @@ Módulo example.py
 Este módulo contiene la funcion get_coordinates que se encarga de obtener las coordenadas de una ciudad a partir de una consulta.
 """
 
+from flask import Flask, request, jsonify
 import requests
 
+app = Flask(__name__)
 
+
+@app.route("/coordinates/<query>", methods=["GET"])
 def get_coordinates(query):
     """
     Este módulo se encarga de obtener las coordenadas de una ciudad a partir de una consulta.
@@ -22,5 +26,11 @@ def get_coordinates(query):
     response = requests.get(api_url, headers=headers)
 
     response_data = response.json()
+    lat = response_data[0]["lat"]
+    lon = response_data[0]["lon"]
 
-    return float(response_data[0]["lat"]), float(response_data[0]["lon"])
+    return jsonify({"success": True, "lat": lat, "lon": lon})
+
+
+if __name__ == "__main__":
+    app.run()
